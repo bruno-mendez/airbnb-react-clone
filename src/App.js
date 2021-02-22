b4583736b10fe5f87d18346c933eab047d8989a3
Bruno Mendez <brunomendez@outlook.com>
2021-02-22 03:47:13 -0300

    adding react router

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import './App.css';
 import Home from './Home';
 import Header from './Header'
 import Footer from './Footer'
+import SearchPage from './SearchPage'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home />
-      <Footer />
+      <Router>
+        <Header/>
 
-     {/* Home */}
-        {/* Header */}
-            {/* Search */}
-        {/* Banner */}
-        {/* Cards */}
-        {/* Footer */}
-      {/* Search Page */}
+        <Switch>
+          <Route path="/search"><SearchPage/></Route>
+          <Route path="/"><Home/></Route> 
+        </Switch>
+
+        <Footer/>
+      </Router>
     </div>
   );
 }
